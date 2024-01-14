2a56073adad3592d7ecb1b060a740ce0d349a291
DarynaSD <dasha.dermanchuk@gmail.com>
2024-01-15 00:42:21 +0200

    remined about auth)

--- routes/api/exercises.js
+++ routes/api/exercises.js
@@ -2,17 +2,17 @@ const express = require('express');
 
 const router = express.Router();
 
-// const { authenticate } = require('../../middlewares');
+const { authenticate } = require('../../middlewares');
 
 const ctrl = require('../../controllers/exercises');
 
 
-// router.get('/', authenticate, ctrl.getAllExercises);
-router.get('/', ctrl.getAllExercises);
+router.get('/', authenticate, ctrl.getAllExercises);
+// router.get('/', ctrl.getAllExercises);
 
 
-// router.get('/filter', authenticate, ctrl.getExercisesCategories);
-router.get('/filter', ctrl.getExercisesCategories);
+router.get('/filter', authenticate, ctrl.getExercisesCategories);
+// router.get('/filter', ctrl.getExercisesCategories);
 
 
 module.exports = router;
