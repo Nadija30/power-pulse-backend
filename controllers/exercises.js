7076480bbf40cb290f89d0f613f4d6159588341d
DarynaSD <dasha.dermanchuk@gmail.com>
2024-01-14 23:12:52 +0200

    fix filter rout

--- controllers/exercises.js
+++ controllers/exercises.js
@@ -1,46 +1,89 @@
-const { ctrlWrapper, HttpError } = require('../helpers');
-const  Exercises  = require ('../models/exercises');
-const  Filter  = require ('../models/filter');
-const exercisesFilterList = require('../models/exercisesFilterList')
+const { ctrlWrapper, HttpError } = require("../helpers");
+const Exercises = require("../models/exercises");
+const Filter = require("../models/filter");
+const exercisesFilterList = require("../models/exercisesFilterList");
 
+// get all
 const getAllExercises = ctrlWrapper(async (req, res) => {
-  const {
-    bodyPart = null,
-    equipment = null,
-    target = null,
-    page = 1,
-    limit = 18,
-  } = req.query;
+  const {filter, page = 1, limit = 18 } = req.query;
   const skip = (page - 1) * limit;
 
-  const baseQuery = {};
-  bodyPart && (baseQuery.bodyPart = { $regex: bodyPart.trim(), $options: 'i' });
-  equipment &&
-    (baseQuery.equipment = { $regex: equipment.trim(), $options: 'i' });
-  target && (baseQuery.target = { $regex: target.trim(), $options: 'i' });
+  const data = await Filter.find({filter}, "", {skip, limit});
 
-  const data = await Exercises.find(baseQuery).skip(skip).limit(limit);
+  // const total = await Filter.countDocuments(Filter);
 
-  const total = await Exercises.countDocuments(baseQuery);
-
-  res.status(200).json({ data, page: +page, limit: +limit, total });
+  res.status(200).json({ data });
 });
 
-          const getExercisesCategories = ctrlWrapper(async (req, res) => {
-            const { filter = null } = req.query;
-            const config = {};
-            filter &&
-              exercisesFilterList[filter] &&
-              (config.filter = exercisesFilterList[filter]);
-            const result = await Filter.find(config);
-            if (!result) {
-                      throw HttpError(404, 'Not found');
-                    }
-            res.json(result);
-          });
-          
+
+const getExercisesCategories = ctrlWrapper(async (req, res) => {
+  const { filter = null } = req.query;
+  const config = {};
+  filter &&
+    exercisesFilterList[filter] &&
+    (config.filter = exercisesFilterList[filter]);
+  const result = await Filter.find(config);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+});
 
 module.exports = {
-    getAllExercises: ctrlWrapper(getAllExercises),
-    getExercisesCategories: ctrlWrapper(getExercisesCategories),
-}
+  getAllExercises: ctrlWrapper(getAllExercises),
+  getExercisesCategories: ctrlWrapper(getExercisesCategories),
+};
+
+
+
+
+
+
+
+
+
+// const { ctrlWrapper, HttpError } = require("../helpers");
+// const Exercises = require("../models/exercises");
+// const Filter = require("../models/filter");
+// const exercisesFilterList = require("../models/exercisesFilterList");
+
+// const getAllExercises = ctrlWrapper(async (req, res) => {
+//   const {
+//     bodyPart = null,
+//     equipment = null,
+//     target = null,
+//     page = 1,
+//     limit = 18,
+//   } = req.query;
+//   const skip = (page - 1) * limit;
+
+//   const baseQuery = {};
+//   bodyPart && (baseQuery.bodyPart = { $regex: bodyPart.trim(), $options: "i" });
+//   equipment &&
+//     (baseQuery.equipment = { $regex: equipment.trim(), $options: "i" });
+//   target && (baseQuery.target = { $regex: target.trim(), $options: "i" });
+
+//   const data = await Exercises.find(baseQuery).skip(skip).limit(limit);
+
+//   const total = await Exercises.countDocuments(baseQuery);
+
+//   res.status(200).json({ data, page: +page, limit: +limit, total });
+// });
+
+// const getExercisesCategories = ctrlWrapper(async (req, res) => {
+//   const { filter = null } = req.query;
+//   const config = {};
+//   filter &&
+//     exercisesFilterList[filter] &&
+//     (config.filter = exercisesFilterList[filter]);
+//   const result = await Filter.find(config);
+//   if (!result) {
+//     throw HttpError(404, "Not found");
+//   }
+//   res.json(result);
+// });
+
+// module.exports = {
+//   getAllExercises: ctrlWrapper(getAllExercises),
+//   getExercisesCategories: ctrlWrapper(getExercisesCategories),
+// };
