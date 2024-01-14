7b871cc98976ed159cbf4d3214913161590031be
DarynaSD <dasha.dermanchuk@gmail.com>
2024-01-15 00:41:24 +0200

    finish exercises filter

--- controllers/exercises.js
+++ controllers/exercises.js
@@ -1,50 +1,74 @@
 const { ctrlWrapper, HttpError } = require("../helpers");
 const Exercises = require("../models/exercises");
 const Filter = require("../models/filter");
-const exercisesFilterList = require("../models/exercisesFilterList");
 
 // get all
 // очікує квері параметр filter зі значенням "Body parts", "Equipment" або "Muscles"
 const getAllExercises = async (req, res) => {
-  const {filter, page = 1, limit = 18 } = req.query;
+  const { filter, page = 1, limit = 18 } = req.query;
   const skip = (page - 1) * limit;
 
-  const data = await Filter.find({filter}, "", {skip, limit});
+  const total = await Filter.countDocuments({});
+  const data = await Filter.find({ filter }, "", { skip, limit });
 
-  // const total = await Filter.countDocuments(Filter);
+  const totalPages = Math.ceil(total / limit);
+  const currentPage = Math.ceil(skip / data.length) + 1;
+  const perPage = limit;
 
-  res.json({ data });
+  res.json({
+    totalPages,
+    currentPage,
+    perPage,
+    result: data,
+  });
 };
-
 
 // get exercises by category
 // очікує квері параметр category зі значенням "Body parts", "Equipment" або "Muscles"
 // і квері параметр specific зі значенням підкатегорії з малої літери ("waist")
 const getExercisesCategories = async (req, res) => {
-  const { filter = null } = req.query;
-  const config = {};
-  filter &&
-    exercisesFilterList[filter] &&
-    (config.filter = exercisesFilterList[filter]);
-  const result = await Filter.find(config);
-  if (!result) {
-    throw HttpError(404, "Not found");
+  const { category, specific, page = 1, limit = 18 } = req.query;
+  const skip = (page - 1) * limit;
+
+  let data;
+  switch (category) {
+    case "Body parts":
+      data = await Exercises.find({ bodyPart: specific });
+      // res.json(data);
+      break;
+
+    case "Equipment":
+      data = await Exercises.find({ equipment: specific });
+      // res.json(data);
+      break;
+
+    case "Muscles":
+      data = await Exercises.find({ target: specific });
+      // res.json(data);
+      break;
+
+    default:
+      throw HttpError(404, "Not found");
   }
-  res.json(result);
+
+  const total = await Exercises.countDocuments({});
+
+  const totalPages = Math.ceil(total / limit);
+  const currentPage = Math.ceil(skip / data.length) + 1;
+  const perPage = limit;
+
+  res.json({
+    totalPages,
+    currentPage,
+    perPage,
+    result: data,
+  });
 };
 
 module.exports = {
   getAllExercises: ctrlWrapper(getAllExercises),
   getExercisesCategories: ctrlWrapper(getExercisesCategories),
 };
-
-
-
-
-
-
-
-
 
 // const { ctrlWrapper, HttpError } = require("../helpers");
 // const Exercises = require("../models/exercises");
