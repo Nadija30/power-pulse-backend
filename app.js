4732cfd113eda8572b6bab9bdd4bac9ceac69cc0
DarynaSD <dasha.dermanchuk@gmail.com>
2024-01-16 15:07:49 +0200

    commented route in app

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const authRouter = require('./routes/api/auth');
 const exercisesRouter = require('./routes/api/exercises');
-const diaryRouter = require('./routes/api/diary');
+// const diaryRouter = require('./routes/api/diary');
 const productsRouter = require('./routes/api/products');
 
 const app = express();
@@ -21,7 +21,7 @@ app.use(express.static('public'));
 app.use('/api/auth', authRouter);
 app.use('/exercises', exercisesRouter);
 
-app.use('/diary', diaryRouter);
+// app.use('/diary', diaryRouter);
 app.use('/products', productsRouter);
 
 app.use((req, res) => {
