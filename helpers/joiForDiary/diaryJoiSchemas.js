1ed03ffbbc82881b40640a6c1a70c34df3e332a7
DarynaSD <dasha.dermanchuk@gmail.com>
2024-01-17 14:47:26 +0200

    add joi validation for diary

---
+++ helpers/joiForDiary/diaryJoiSchemas.js
@@ -0,0 +1,45 @@
+const Joi = require('joi');
+
+const validDatePattern = /^\d{4}-\d{2}-\d{2}$/;
+
+
+const addProductJoiSchema = Joi.object({
+  productId: Joi.string().required().messages({
+    'any.required': "Missing required productId field",
+  }),
+  date: Joi.string().pattern(validDatePattern).required().messages({
+    'any.pattern': "Date should be 'yyyy-mm-dd' format",
+    'any.required': "Missing required date field",
+  }),
+  grams: Joi.number().min(1).required().messages({
+    'any.required': 'Missing required grams field',
+  }),
+  calories: Joi.number().min(1).required().messages({
+    'any.required': 'Missing required calories field',
+  }),
+});
+
+
+const addExerciseJoiSchema = Joi.object({
+  exerciseId: Joi.string().required().messages({
+    'any.required': "Missing required exerciseId field",
+  }),
+  date: Joi.string().pattern(validDatePattern).required().messages({
+    'any.pattern': "Date should be 'yyyy-mm-dd' format",
+    'any.required': "Missing required date field",
+  }),
+  duration: Joi.number().min(1).required().messages({
+    'any.required': 'Missing required duration field',
+  }),
+  burnedCalories: Joi.number().min(1).required().messages({
+    'any.required': 'Missing required burnedCalories field',
+  }),
+});
+
+
+const schemas = {
+  addProductJoiSchema,
+  addExerciseJoiSchema,
+};
+
+module.exports = { schemas };
